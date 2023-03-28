0ed658da46bdfe4742ce5dab94d1b3d9a667a0a8
Nezametdinov E. Ildus <neil.log@gmail.com>
2023-03-28 20:36:35 +0300

    Add memory management utilities

---
+++ src/memory.c
@@ -0,0 +1,35 @@
+// Copyright Nezametdinov E. Ildus 2023.
+// Distributed under the GNU General Public License, Version 3.
+// (See accompanying file LICENSE_GPL_3_0.txt or copy at
+// https://www.gnu.org/licenses/gpl-3.0.txt)
+//
+#include "memory.h"
+#include <stdlib.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Allocation/deallocation interface implementation.
+////////////////////////////////////////////////////////////////////////////////
+
+struct rose_memory
+rose_allocate(size_t size) {
+    // Initialize an empty memory object.
+    struct rose_memory memory = {};
+
+    // Allocate memory.
+    if((size != 0) && ((memory.data = malloc(size)) != NULL)) {
+        memory.size = size;
+    }
+
+    // Return result of allocation.
+    return memory;
+}
+
+void
+rose_free(struct rose_memory* memory) {
+    if(memory != NULL) {
+        free(memory->data);
+
+        memory->data = NULL;
+        memory->size = 0;
+    }
+}
---
+++ src/memory.h
@@ -0,0 +1,30 @@
+// Copyright Nezametdinov E. Ildus 2023.
+// Distributed under the GNU General Public License, Version 3.
+// (See accompanying file LICENSE_GPL_3_0.txt or copy at
+// https://www.gnu.org/licenses/gpl-3.0.txt)
+//
+#ifndef H_2AE0EAD7C9DB4AA4AFE79218D8FAF38E
+#define H_2AE0EAD7C9DB4AA4AFE79218D8FAF38E
+
+#include <stddef.h>
+
+////////////////////////////////////////////////////////////////////////////////
+// Memory definition.
+////////////////////////////////////////////////////////////////////////////////
+
+struct rose_memory {
+    unsigned char* data;
+    size_t size;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Allocation/deallocation interface.
+////////////////////////////////////////////////////////////////////////////////
+
+struct rose_memory
+rose_allocate(size_t size);
+
+void
+rose_free(struct rose_memory* memory);
+
+#endif // H_2AE0EAD7C9DB4AA4AFE79218D8FAF38E
